3f3a2b5c977292bc3d8922261fe37a693c69ca17
Raghu Betina <raghu@firstdraft.com>
2018-12-12 00:30:08 +0000

    Whitelist columns in DishHasIngredient for ActiveAdmin

--- app/admin/dish_has_ingredients.rb
+++ app/admin/dish_has_ingredients.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register DishHasIngredient do
+
+ permit_params :dish_id, :ingredient_id, :quantity, :dish_ingredient_unit
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
